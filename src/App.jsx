ae07bed23df767c2147f55c7ce2348411cc3bf82
julian2238 <72612081+julian2238@users.noreply.github.com>
2025-10-05 18:51:53 -0500

    Se ajusta diseño de iniciar sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
 import { Route, Routes } from 'react-router'
 import Login from './pages/login/Login'
 import PrivateRoutes from './routes/PrivateRoutes'
 import Dashboard from './pages/dashboard/Dashboard'
 
 const App = () => {
-  return (
-    <>
-        <Routes>
-            <Route path='/' element={<Login />}/>
-            <Route 
-                path='dashboard'
-                element={
-                    <PrivateRoutes user={"data"}>
-                        <Dashboard />
-                    </PrivateRoutes>
-                }
-            />
-        </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route
+                    path='dashboard'
+                    element={
+                        <PrivateRoutes user={'data'}>
+                            <Dashboard />
+                        </PrivateRoutes>
+                    }
+                />
+            </Routes>
+        </>
+    )
 }
 
 export default App
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,55 +1,88 @@
-import emailIcon from '/src/assets/email.svg'
-import passwordIcon from '/src/assets/password.svg'
+import { useForm } from 'react-hook-form'
 import background from '../../assets/backgroundLogin.jpg'
 import './Login.css'
 
+const Form = ({ onSubmit }) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    console.log('errors', errors)
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h1 className='title'>Bienvenido</h1>
+            <p className='text'>Ingresa tus datos</p>
+            <div className='containerForm'>
+                <div>
+                    <label className='label' htmlFor='email'>
+                        Correo
+                    </label>
+                    <input
+                        {...register('email', {
+                            required: {
+                                value: true,
+                                message: '*Este campo es obligatorio',
+                            },
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: '*Ingresa un correo válido',
+                            },
+                        })}
+                        className='input'
+                        id='email'
+                        type='email'
+                        placeholder='Ingresa tu correo'
+                    />
+                    {errors.email && <span className='errorText'>{errors.email.message}</span>}
+                </div>
+                <div style={{ marginTop: '1rem' }}>
+                    <label className='label' htmlFor='password'>
+                        Contraseña
+                    </label>
+                    <input
+                        {...register('password', {
+                            required: {
+                                value: true,
+                                message: '*Este campo es obligatorio',
+                            },
+                        })}
+                        className='input'
+                        id='password'
+                        type='password'
+                        placeholder='Ingresa tu contraseña'
+                    />
+                    {errors.password && (
+                        <span className='errorText'>{errors.password.message}</span>
+                    )}
+                </div>
+                <div className='containerForgotPassword'>
+                    <button className='btnForgotPassword'>Olvidé mi contraseña</button>
+                </div>
+                <button type='submit' className='btnSignIn'>
+                    Iniciar Sesión
+                </button>
+            </div>
+        </form>
+    )
+}
+
 const Login = () => {
-    const onSubmit = (e) => {
-        e.preventDefault()
+    const onSubmit = (data) => {
+        console.log(data)
     }
 
     return (
         <div className='containerMainLogin'>
             <div className='containerLogin'>
-                <form action=''>
-                    <p className='textLogin'>Bienvenido</p>
-                    <div className='inputLogin'>
-                        <img src={emailIcon} />
-                        <input type='text' placeholder='Correo' />
-                    </div>
-                    <div className='inputLogin'>
-                        <img src={passwordIcon} />
-
-                        <input type='text' placeholder='Contraseña' />
-                    </div>
-                    <button onClick={onSubmit} className='submitLogin'>
-                        Iniciar Sesión
-                    </button>
-                </form>
+                <Form onSubmit={onSubmit} />
             </div>
-            <div style={{ backgroundImage: `url(${background})` }} className='containerImage'></div>
+            <div className='containerImage'>
+                <img src={background} alt='login-photo' />
+            </div>
         </div>
-
-        // <div style={{ backgroundImage: `url(${background})`}} className='main-login'>
-        //     <div className='containerLogin'>
-        //         <div className="headerLogin">
-        //             <div className="textLogin">Iniciar Sesión</div>
-        //             <div className="underlineLogin"></div>
-        //         </div>
-        //         <div className="inputsLogin">
-        //             <div className="inputLogin">
-        //                 <img src={emailIcon} />
-        //                 <input type="text" />
-        //             </div>
-        //             <div className="inputLogin">
-        //                 <img src={passwordIcon} height={32} width={32} />
-
-        //                 <input type="text" />
-        //             </div>
-        //         </div>
-        //         <div className="submitLogin">Iniciar Sesión</div>
-        //     </div>
-        // </div>
     )
 }
 
